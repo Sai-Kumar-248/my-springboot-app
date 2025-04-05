df94ae9c8bebfa669868bdf9f512ed8c8b3ec9f6
Sai <ksaikumar5148@gmail.com>
2025-04-05 08:06:45 +0530

    db

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,31 +11,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.User;
+import com.example.demo.repoi.UserRepository;
 
-/*import com.example.demo.repoi.UserRepository;
-*/
 @CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/users")
 public class UserController {
-	/*
-	 * @Autowired private UserRepository userRepository;
-	 */
 
-    @GetMapping
-    public List<User> getUsers() {
-    	List<User> usersList = new ArrayList<User>();
-    	User user = new User();
-    	user.setId((long) 1);
-    	user.setName("saik");
-    	user.setEmail("sai@gmail.com");
-    	usersList.add(user);
-    	
-        return usersList;
-    }
+	@Autowired
+	private UserRepository userRepository;
 
-	/*
-	 * @PostMapping public User createUser(@RequestBody User user) { return
-	 * userRepository.save(user); }
-	 */
+	@GetMapping
+	public List<User> getUsers() {
+//		List<User> usersList = new ArrayList<User>();
+//		User user = new User();
+//		user.setId((long) 1);
+//		user.setName("saik");
+//		user.setEmail("sai@gmail.com");
+//		usersList.add(user);
+
+		return userRepository.findAll();
+	}
+
+	@PostMapping
+	public User createUser(@RequestBody User user) {
+		return userRepository.save(user);
+	}
+
 }
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,42 +1,50 @@
 package com.example.demo.entity;
 
-/*import jakarta.persistence.Entity;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;*/
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-//@Entity
+@Entity
 @Data
+@Table(name = "users")
+@NoArgsConstructor @AllArgsConstructor
 public class User {
-	/*
-	 * @Id
-	 * 
-	 * @GeneratedValue(strategy = GenerationType.IDENTITY)
-	 */
-    private Long id;
-    private String name;
-    private String email;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
 
-    // Constructors, Getters, and Setters
-    
-    
+	// Constructors, Getters, and Setters
+
 }
